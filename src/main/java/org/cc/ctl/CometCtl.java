0f25a2d3b1bcdab8080df0708e0ded79f71c8ce5
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2012-12-22 19:39:59 +0400

    fix home page forever

--- src/main/java/org/cc/ctl/CometCtl.java
+++ src/main/java/org/cc/ctl/CometCtl.java
@@ -2,15 +2,19 @@ package org.cc.ctl;
 
 import org.cc.response.CloudResponse;
 import org.cc.ent.RequestStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 /**
  * Client calls this method get responses.
-
+ * <p/>
  * Daneel Yaitskov
  */
+@Controller
 public class CometCtl {
 
     public RequestStatus getStatus(@RequestParam("request") int requestId) {
@@ -23,5 +27,12 @@ public class CometCtl {
      */
     public List<CloudResponse> getNew(@RequestParam("last") int lastId) {
         return null;
+    }
+
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String homePage() {
+        return "This is Cloud48";
     }
 }
