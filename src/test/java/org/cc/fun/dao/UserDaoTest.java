0ce17e5cc4ce5d5f00045b8b7d76be9c2a915534
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2012-12-22 14:31:04 +0400

    UserDao#findByLogin test is ok

--- src/test/java/org/cc/fun/dao/UserDaoTest.java
+++ src/test/java/org/cc/fun/dao/UserDaoTest.java
@@ -1,6 +1,8 @@
 package org.cc.fun.dao;
 
+import junit.framework.Assert;
 import org.cc.dao.UserDao;
+import org.cc.ent.User;
 import org.cc.util.BaseDao;
 import org.cc.util.DbCleaner;
 import org.junit.Before;
@@ -9,6 +11,9 @@ import org.junit.runner.RunWith;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.Resource;
 import javax.persistence.NoResultException;
@@ -23,8 +28,33 @@ public class UserDaoTest extends BaseDao {
     @Resource
     UserDao userDao;
 
+    @Resource(name = "ttReuse")
+    TransactionTemplate ttReuse;
+
     @Test
     public void testFindByLogin() {
+        ttReuse.execute(
+                new TransactionCallbackWithoutResult() {
+                    @Override
+                    protected void doInTransactionWithoutResult(TransactionStatus status) {
+                        User u = new User();
+                        u.setLogin("u1");
+                        u.setPass("p1");
+
+                        userDao.save(u);
+
+                        u = new User();
+
+                        u.setLogin("u2");
+                        u.setPass("p2");
+
+                        userDao.save(u);
+                    }
+                }
+        );
+
+        Assert.assertNotNull(userDao.findByLogin("u1"));
+        Assert.assertNotNull(userDao.findByLogin("u2"));
     }
 
     @Test(expected = EmptyResultDataAccessException.class)
